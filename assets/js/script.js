dfe8ad949b9e451647be86bb38f41983aa29cddd
Carlos Eduardo <carlos.eduardo.cs@icloud.com>
2023-03-12 12:49:39 -0300

    :tada: Commit Inicial

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const projects = ["resume-page"];
+
+const list = document.getElementById("list");
+
+projects.forEach((project) => {
+	const listItem = document.createElement("li");
+	const link = document.createElement("a");
+	link.href = `/${project}/index.html`;
+	link.innerText = `${formatProjectName(project)}`;
+
+	const img = document.createElement("img");
+	img.src = `/${project}/design/desktop-design.jpg`;
+
+	link.prepend(img);
+	listItem.appendChild(link);
+	list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+	return name
+		.split("-")
+		.map((word) => word[0].toUpperCase() + word.slice(1))
+		.join(" ");
+}
---
+++ resume-page/README.md
@@ -0,0 +1,14 @@
+### Sobre o projeto
+
+#### Finalidade
+
+- Exibir informações necessárias a respeito de minhas formações e experiências adquiridas ao longo dos anos com tecnologia.
+- Dentro do arquivo existe uma função para que seja feito o download pdf do resumo do mesmo, para facilitar a usabilidade de recrutadores.
+
+#### Atualizações
+
+- Devido nunca pararmos de estudar o arquivo sempre terá atualizações e ajustes.
+
+#### Encorajamento
+
+- Faça um fork nesse projeto e construa algo que acredite que possa melhorar ele, afinal sempre é bem vindo um novo aprendizado e uma nova visão.
